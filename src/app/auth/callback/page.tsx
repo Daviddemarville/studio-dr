95864c7711d5f9551b261bd4d6b7ba8e1ae139f9
David de Marville <de.marville.david@gmail.com>
2025-11-17 15:57:10 +0100

    add fix

--- src/app/auth/callback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function CallbackPage({ searchParams }: { searchParams: any }) {
-  // Recompose la query string complète
-  const qs = new URLSearchParams(searchParams).toString();
-
-  // Redirection vers ton API route (celle qui contient ton callback complet)
-  redirect(`/api/auth/callback?${qs}`);
-}
