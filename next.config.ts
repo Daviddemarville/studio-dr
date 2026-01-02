d3ee26fd6c392ef6ce49aaf905e87687513070e9
rahmoundif <rahmoun.dif@gmail.com>
2026-01-02 21:05:58 +0100

    Initialisation des tests pour Auth

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "cdn.discordapp.com" },
       { protocol: "https", hostname: "media.discordapp.net" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "rahmoundif.dev" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "lh4.googleusercontent.com" },
       { protocol: "https", hostname: "lh5.googleusercontent.com" },
